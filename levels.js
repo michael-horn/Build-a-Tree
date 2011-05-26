38f91c69625c19db7157e69985e4ff21b9ad6b60
Michael Horn <michael-horn@northwestern.edu>
2011-05-25 23:22:27 -0500

    added trait info

--- levels.js
+++ levels.js
@@ -5,8 +5,8 @@ const LEVELS = [
    name : "Level 1", 
    
    taxa : [
-      { id : 0, parent_id : null, name : "Eukaryotes", depth : 2, image : null },
-      { id : 1, parent_id : 0, name : "Animals", depth : 1, image : null },
+      { id : 0, parent_id : null, name : "Eukaryotes", depth : 2, image : null, trait : "complex cellular structure" },
+      { id : 1, parent_id : 0, name : "Animals", depth : 1, image : null, trait : "animals" },
       { id : 2, parent_id : 0, name : "Plants", depth : 0, image : "plant" },
       { id : 3, parent_id : 1, name : "Birds", depth : 0, image : "bird" },
       { id : 4, parent_id : 1, name : "Lizards", depth : 0, image : "lizard" }
@@ -17,8 +17,8 @@ const LEVELS = [
    name : "Level 2", 
    
    taxa : [
-      { id : 0, parent_id : null, name : "Amniotes", depth : 2, image : null },
-      { id : 1, parent_id : 0, name : "Mammals", depth : 1, image : null },
+      { id : 0, parent_id : null, name : "Vertebrates", depth : 2, image : null, trait : "animals with backbones" },
+      { id : 1, parent_id : 0, name : "Mammals", depth : 1, image : null, trait : "mammals" },
       { id : 2, parent_id : 1, name : "Humans", depth : 0, image : "human" },
       { id : 3, parent_id : 0, name : "Birds", depth : 0, image : "bird" },
       { id : 4, parent_id : 1, name : "Bats", depth : 0, image : "bat" }
@@ -29,8 +29,8 @@ const LEVELS = [
    name : "Level 3", 
    
    taxa : [
-      { id : 0, parent_id : null, name : "Eukaryotes", depth : 2, image : null },
-      { id : 1, parent_id : 0, name : "Animals and Fungi", depth : 1, image : null },
+      { id : 0, parent_id : null, name : "Eukaryotes", depth : 2, image : null, trait : "complex cellular structure" },
+      { id : 1, parent_id : 0, name : "Animals and Fungi", depth : 1, image : null, trait : null },
       { id : 2, parent_id : 0, name : "Plants", depth : 0, image : "plant" },
       { id : 3, parent_id : 1, name : "Fungi", depth : 0, image : "fungus" },
       { id : 4, parent_id : 1, name : "Humans", depth : 0, image : "human" }
@@ -41,8 +41,8 @@ const LEVELS = [
    name : "Level 4", 
    
    taxa : [
-      { id : 0, parent_id : null, name : "Arthropods", depth : 2, image : null },
-      { id : 1, parent_id : 0, name : "Arachnids", depth : 1, image : null },
+      { id : 0, parent_id : null, name : "Arthropods", depth : 2, image : null, trait : "exoskeletons" },
+      { id : 1, parent_id : 0, name : "Arachnids", depth : 1, image : null, trait : "eight legs" },
       { id : 2, parent_id : 1, name : "Scorpions", depth : 0, image : "scorpion" },
       { id : 3, parent_id : 1, name : "Spiders", depth : 0, image : "spider" },
       { id : 4, parent_id : 0, name : "Insects", depth : 0, image : "butterfly" }
@@ -53,11 +53,11 @@ const LEVELS = [
    name : "Level 5",
    
    taxa : [
-      { id : 0, parent_id : null, name : "Animals", depth : 3, image : null },
-      { id : 1, parent_id : 0, name : "Vertebrates", depth : 2, image : null },
-      { id : 2, parent_id : 0, name : "Arthropods", depth : 1, image : null },
-      { id : 3, parent_id : 1, name : "Mammals", depth : 1, image : null },
-      { id : 4, parent_id : 1, name : "Dinosaurs and Birds", depth : 1, image : null },
+      { id : 0, parent_id : null, name : "Animals", depth : 3, image : null, trait : "animals" },
+      { id : 1, parent_id : 0, name : "Vertebrates", depth : 2, image : null, trait : "animals with backbones" },
+      { id : 2, parent_id : 0, name : "Arthropods", depth : 1, image : null, trait : "exoskeletons" },
+      { id : 3, parent_id : 1, name : "Mammals", depth : 1, image : null, trait : "mammals" },
+      { id : 4, parent_id : 1, name : "Dinosaurs and Birds", depth : 1, image : null, trait : "wishbones" },
       { id : 5, parent_id : 4, name : "Birds", depth : 0, image : "bird" },
       { id : 6, parent_id : 4, name : "T. Rex", depth : 0, image : "trex" },
       { id : 7, parent_id : 3, name : "Giraffes", depth : 0, image : "giraffe" },
@@ -71,11 +71,11 @@ const LEVELS = [
    name : "Level 6",
    
    taxa : [
-      { id : 0, parent_id : null, name : "Animals", depth : 4, image : null },
+      { id : 0, parent_id : null, name : "Animals", depth : 4, image : null, trait : "animals" },
       { id : 1, parent_id : 0, name : "Vertebrates", depth : 3, image : null, trait : "animals with backbones" },
       { id : 2, parent_id : 0, name : "Arthropods", depth : 1, image : null, trait : "exoskeleton" },
       { id : 3, parent_id : 1, name : "Amniotes", depth : 2, image : null, trait : "amniotic egg" },
-      { id : 4, parent_id : 3, name : "Diapsids", depth : 1, image : null, trait : "diapsids" },
+      { id : 4, parent_id : 3, name : "Diapsids", depth : 1, image : null, trait : "" },
       { id : 5, parent_id : 1, name : "Frogs", depth : 0, image : "frog", hint : "Frogs are a type of <i>amphibian</i>. Amphibians belong to a group of animals called vertebrates." },
       { id : 6, parent_id : 3, name : "Bats", depth : 0, image : "bat", hint: "Bats are a type of mammal. Mammals belong to a group of animals called <i>diapsids</i> that also includes birds and lizards."  },
       { id : 7, parent_id : 4, name : "Birds", depth : 0, image : "bird", hint : "Birds and lizards belong to a group of animals called <i>diapsids</i>. Crocodiles and dinosaurs are also diapsids." },
@@ -89,14 +89,14 @@ const LEVELS = [
    name : "Level 7",
    
    taxa : [
-      { id : 0, parent_id : null, name : "Animals", depth : 5, image : null },
-      { id : 1, parent_id : 0, name : "Vertebrates", depth : 4, image : null },
-      { id : 2, parent_id : 0, name : "Arthropods", depth : 2, image : null },
-      { id : 3, parent_id : 2, name : "Arachnids", depth : 1, image : null },
-      { id : 4, parent_id : 1, name : "Amniotes", depth : 3, image : null },
-      { id : 5, parent_id : 4, name : "Mammals", depth : 1, image : null },
-      { id : 6, parent_id : 4, name : "Reptilia", depth : 2, image : null },
-      { id : 7, parent_id : 6, name : "Dinosaurs and Birds", depth : 1, image : null },
+      { id : 0, parent_id : null, name : "Animals", depth : 5, image : null, trait : "animals" },
+      { id : 1, parent_id : 0, name : "Vertebrates", depth : 4, image : null, trait : "backbones" },
+      { id : 2, parent_id : 0, name : "Arthropods", depth : 2, image : null, trait : "exoskeletons" },
+      { id : 3, parent_id : 2, name : "Arachnids", depth : 1, image : null, trait : "eight legs" },
+      { id : 4, parent_id : 1, name : "Amniotes", depth : 3, image : null, trait : "amniotic egg" },
+      { id : 5, parent_id : 4, name : "Mammals", depth : 1, image : null, trait : "mammals" },
+      { id : 6, parent_id : 4, name : "Diapsids", depth : 2, image : null, trait : "diapsids" },
+      { id : 7, parent_id : 6, name : "Dinosaurs and Birds", depth : 1, image : null, trait : "wishbones" },
       { id : 8, parent_id : 1, name : "Frogs", depth : 0, image : "frog" },
       { id : 9, parent_id : 6, name : "Lizards", depth : 0, image : "lizard" },
       { id : 10, parent_id : 7, name : "Birds", depth : 0, image : "bird" },
