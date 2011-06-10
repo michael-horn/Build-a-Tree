bb57ab008797ad8e4e5deff65494fb292925c534
Michael Horn <michael-horn@northwestern.edu>
2011-06-10 04:32:00 -0500

    added get/set functions for properties

--- button.js
+++ button.js
@@ -1,3 +1,17 @@
+/*
+ * Build-a-Tree
+ * Life on Earth Project (http://sdr.seas.harvard.edu/content/life-earth)
+ *
+ * Michael S. Horn
+ * Northwestern University
+ * michael-horn@northwestern.edu
+ * Copyright 2011, Michael S. Horn
+ *
+ * This project was funded by the National Science Foundation (grant 1010889).
+ * Any opinions, findings and conclusions or recommendations expressed in this
+ * material are those of the author(s) and do not necessarily reflect the views
+ * of the National Science Foundation (NSF).
+ */
 function Button(x, y, w, h) {
 
    this.x = x;
@@ -14,6 +28,27 @@ function Button(x, y, w, h) {
       this.y = cy - this.h/2;
    }
    
+   this.setPosition = function(x, y) {
+      this.x = x;
+      this.y = y;
+   }
+   
+   this.getCenterX = function() {
+      return this.x + this.w/2;
+   }
+   
+   this.getCenterY = function() {
+      return this.y + this.h/2;
+   }
+   
+   this.getWidth = function() {
+      return this.w;
+   }
+   
+   this.getHeight = function() {
+      return this.h;
+   }
+   
    this.draw = function(g) { }
    
    this.action = function() { }
@@ -24,6 +59,14 @@ function Button(x, y, w, h) {
    
    this.isOver = function() {
       return this.containsTouch(this.touch);
+   }
+   
+   this.isVisible = function() {
+      return this.visible;
+   }
+   
+   this.setVisible = function(v) {
+      this.visible = v;
    }
    
 //==========================================================================
@@ -46,6 +89,7 @@ function Button(x, y, w, h) {
    
    this.touchUp = function() {
       if (this.isDown() && this.containsTouch(this.touch)) {
+         this.down = false;  // in case action logic depends on down state
          this.action();
       }
       this.down = false;
