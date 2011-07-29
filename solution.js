12d9d12745d6ec37daafef0a41b19f479ee838f3
Michael Horn <michael-horn@northwestern.edu>
2011-07-29 10:50:18 -0500

    initial checkin

---
+++ solution.js
@@ -0,0 +1,128 @@
+/*
+ * Build-a-Tree
+ * Life on Earth Project (http://sdr.seas.harvard.edu/content/life-earth)
+ *
+ * Michael S. Horn
+ * Northwestern University
+ * michael-horn@northwestern.edu
+ * Copyright 2011, Michael S. Horn
+ *
+ * This project was funded by the National Science Foundation (grant 1010889).
+ * Any opinions, findings and conclusions or recommendations expressed in this
+ * material are those of the author(s) and do not necessarily reflect the views
+ * of the National Science Foundation (NSF).
+ */
+
+//==============================================================
+// A solution box with the scientist tree
+//==============================================================
+function SolutionBox(solution) {
+   
+   this.solution = solution;
+   this.open = false;
+   this.down = false;
+   this.over = false;
+   
+   this.tween = new Tween();
+   
+   this.delta = { x : 0, y : 0 };
+   this.w = Math.max(200, 55 + 30 * solution.getTipCount());
+   this.h = 110 + solution.getRoot().getDepth() * 20;
+   this.x = canvas.width / 2 - this.w / 2;
+   this.y = 30 - this.h;
+   
+   var s = this;
+   this.tween.setActionCallback(function(val) { s._taction(val) });
+   this.tween.setEndCallback(function() { s._tdone(); } );
+   this.tween.setDuration(300);
+   this.tween.setFunction(TWEEN_DECAY);
+   
+   
+   this.draw = function(g) {
+      var sx = this.x;
+      var sy = this.y;
+      var sw = this.w;
+      var sh = this.h;
+      roundRect(g, sx, sy, sw, sh, 20);
+      if (this.down && this.over) {
+         g.fillStyle = "rgba(255, 255, 255, 0.4)";
+      } else {
+         g.fillStyle = "rgba(255, 255, 255, 0.2)";
+      }
+      g.strokeStyle = "white";
+      g.stroke();
+      g.fill();
+      g.fillStyle = "white";
+      solution.drawSmallTree(g, sx + 5, sy + 20, sw - 20, sh - 45);
+      g.fillStyle = "white";
+      g.textAlign = "center";
+      g.textBaseline = "bottom";
+      g.font = "12pt Tahoma, Arial, sans-serif";
+      g.beginPath(); 
+      g.fillText("Scientist Tree", sx + sw/2, sy + sh - 5);
+      
+      // Arrow
+      var tx = sx + sw - 35;
+      var ty = sy + sh - 14;
+      var dy = this.open? 5 : -5;
+      g.beginPath();
+      g.fillStyle = "white";
+      g.moveTo(tx, ty - dy);
+      g.lineTo(tx - 6, ty + dy);
+      g.lineTo(tx + 6, ty + dy);
+      g.closePath();
+      g.fill();
+   }
+   
+   this.animate = function() {
+      
+   }
+
+   
+//----------------------------------------------------------------------
+// TOUCH FUNCTIONS
+//----------------------------------------------------------------------
+   this.containsTouch = function(tp) {
+      return (tp.x >= this.x && 
+              tp.y >= this.y &&
+              tp.x <= this.x + this.w &&
+              tp.y <= this.y + this.h);
+   }
+   
+   this.touchDown = function(tp) {
+      this.down = true;
+      this.over = true;
+   }
+   
+   this.touchUp = function() {
+      if (this.over) {
+         if (this.open) {
+            this.tween.clearControlPoints();
+            this.tween.setStart(-18);
+            this.tween.setEnd(30 - this.h);
+            this.open = false;
+         } else {
+            this.tween.clearControlPoints();
+            this.tween.setStart(30 - this.h);
+            this.tween.setEnd(-18);
+            this.open = true;
+         }
+         this.tween.play();
+      }
+      this.over = false;
+      this.down = false;
+   }
+   
+   this.touchDrag = function(tp) {
+      this.over = this.containsTouch(tp);
+   }
+   
+   this._taction = function(value) {
+      this.y = value;
+      console.log(value);
+   }
+   
+   this._tdone = function() {
+   }
+   
+}
