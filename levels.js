b39c17576bdf34e26464463d685196c570128b56
Michael Horn <michael-horn@northwestern.edu>
2011-07-29 23:11:13 -0500

    tried to fix up some of the traits and grouop names

--- levels.js
+++ levels.js
@@ -6,7 +6,7 @@ const LEVELS = [
    
    taxa : [
       { id : 0, parent_id : null, name : "Eukaryotes", depth : 2, tag : "eukaryotes", trait : "complex cellular structure" },
-      { id : 1, parent_id : 0, name : "Animals", depth : 1, tag : "animals", trait : "animal" },
+      { id : 1, parent_id : 0, name : "Animals", depth : 1, tag : "animals", trait : null },
       { id : 2, parent_id : 0, name : "Plants", depth : 0, tag : "plant" },
       { id : 3, parent_id : 1, name : "Birds", depth : 0, tag : "bird" },
       { id : 4, parent_id : 1, name : "Lizards", depth : 0, tag : "lizard" }
@@ -17,8 +17,8 @@ const LEVELS = [
    name : "Level 2", 
    
    taxa : [
-      { id : 0, parent_id : null, name : "Vertebrates", depth : 2, tag : "vertebrates", trait : "animal with backbone" },
-      { id : 1, parent_id : 0, name : "Mammals", depth : 1, tag : "mammals", trait : "mammal" },
+      { id : 0, parent_id : null, name : "Vertebrates", depth : 2, tag : "vertebrates", trait : "internal skeleton" },
+      { id : 1, parent_id : 0, name : "Mammals", depth : 1, tag : "mammals", trait : "hair" },
       { id : 2, parent_id : 1, name : "Humans", depth : 0, tag : "human" },
       { id : 3, parent_id : 0, name : "Birds", depth : 0, tag : "bird" },
       { id : 4, parent_id : 1, name : "Bats", depth : 0, tag : "bat" }
@@ -53,11 +53,11 @@ const LEVELS = [
    name : "Level 5",
    
    taxa : [
-      { id : 0, parent_id : null, name : "Animals", depth : 3, tag : "animals", trait : "animal" },
-      { id : 1, parent_id : 0, name : "Vertebrates", depth : 2, tag : "vertebrates", trait : "backbone" },
+      { id : 0, parent_id : null, name : "Animals", depth : 3, tag : "animals", trait : null },
+      { id : 1, parent_id : 0, name : "Vertebrates", depth : 2, tag : "vertebrates", trait : "internal skeleton" },
       { id : 2, parent_id : 0, name : "Arthropods", depth : 1, tag : "arthropods", trait : "exoskeleton" },
-      { id : 3, parent_id : 1, name : "Mammals", depth : 1, tag : "mammals", trait : "mammal" },
-      { id : 4, parent_id : 1, name : "Dinosaurs and Birds", depth : 1, tag : "dinobirds", trait : "wishbone" },
+      { id : 3, parent_id : 1, name : "Mammals", depth : 1, tag : "mammals", trait : "hair" },
+      { id : 4, parent_id : 1, name : "Theropods", depth : 1, tag : "dinobirds", trait : "wishbone" },
       { id : 5, parent_id : 4, name : "Birds", depth : 0, tag : "bird" },
       { id : 6, parent_id : 4, name : "T. Rex", depth : 0, tag : "trex" },
       { id : 7, parent_id : 3, name : "Giraffes", depth : 0, tag : "giraffe" },
@@ -71,11 +71,11 @@ const LEVELS = [
    name : "Level 6",
    
    taxa : [
-      { id : 0, parent_id : null, name : "Animals", depth : 4, tag : "animals", trait : "animal" },
-      { id : 1, parent_id : 0, name : "Vertebrates", depth : 3, tag : "vertebrates", trait : "backbone" },
+      { id : 0, parent_id : null, name : "Animals", depth : 4, tag : "animals", trait : null },
+      { id : 1, parent_id : 0, name : "Vertebrates", depth : 3, tag : "vertebrates", trait : "internal skeleton" },
       { id : 2, parent_id : 0, name : "Arthropods", depth : 1, tag : "arthropods", trait : "exoskeleton" },
       { id : 3, parent_id : 1, name : "Amniotes", depth : 2, tag : "amniotes", trait : "amniotic egg" },
-      { id : 4, parent_id : 3, name : "Diapsids", depth : 1, tag : "diapsids", trait : "diapsid" },
+      { id : 4, parent_id : 3, name : "Diapsids", depth : 1, tag : "diapsids", trait : "long lower arm bone" },
       { id : 5, parent_id : 1, name : "Frogs", depth : 0, tag : "frog", hint : "Frogs are a type of <i>amphibian</i>. Amphibians belong to a group of animals called vertebrates." },
       { id : 6, parent_id : 3, name : "Bats", depth : 0, tag : "bat", hint: "Bats are a type of mammal. Mammals belong to a group of animals called <i>diapsids</i> that also includes birds and lizards."  },
       { id : 7, parent_id : 4, name : "Birds", depth : 0, tag : "bird", hint : "Birds and lizards belong to a group of animals called <i>diapsids</i>. Crocodiles and dinosaurs are also diapsids." },
@@ -89,14 +89,14 @@ const LEVELS = [
    name : "Level 7",
    
    taxa : [
-      { id : 0, parent_id : null, name : "Animals", depth : 5, tag : "animals", trait : "animal" },
-      { id : 1, parent_id : 0, name : "Vertebrates", depth : 4, tag : "vertebrates", trait : "backbone" },
+      { id : 0, parent_id : null, name : "Animals", depth : 5, tag : "animals", trait : null },
+      { id : 1, parent_id : 0, name : "Vertebrates", depth : 4, tag : "vertebrates", trait : "internal skeleton" },
       { id : 2, parent_id : 0, name : "Arthropods", depth : 2, tag : "arthropods", trait : "exoskeleton" },
       { id : 3, parent_id : 2, name : "Arachnids", depth : 1, tag : "arachnids", trait : "eight legs" },
       { id : 4, parent_id : 1, name : "Amniotes", depth : 3, tag : "amniotes", trait : "amniotic egg" },
-      { id : 5, parent_id : 4, name : "Mammals", depth : 1, tag : "mammals", trait : "mammal" },
-      { id : 6, parent_id : 4, name : "Diapsids", depth : 2, tag : "diapsids", trait : "diapsid" },
-      { id : 7, parent_id : 6, name : "Dinosaurs and Birds", depth : 1, tag : "dinobirds", trait : "wishbone" },
+      { id : 5, parent_id : 4, name : "Mammals", depth : 1, tag : "mammals", trait : "hair" },
+      { id : 6, parent_id : 4, name : "Diapsids", depth : 2, tag : "diapsids", trait : "long lower arm bone" },
+      { id : 7, parent_id : 6, name : "Theropods", depth : 1, tag : "dinobirds", trait : "wishbone" },
       { id : 8, parent_id : 1, name : "Frogs", depth : 0, tag : "frog" },
       { id : 9, parent_id : 6, name : "Lizards", depth : 0, tag : "lizard" },
       { id : 10, parent_id : 7, name : "Birds", depth : 0, tag : "bird" },
