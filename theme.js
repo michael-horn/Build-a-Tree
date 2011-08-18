f51ebc51fefb2c17184169c7a2054731a97e5bcb
Michael Horn <michael-horn@northwestern.edu>
2011-08-17 22:23:21 -0500

    Theme colors

---
+++ theme.js
@@ -0,0 +1,38 @@
+/*
+ * Build-a-Tree
+ * Life on Earth Project (http://sdr.seas.harvard.edu/content/life-earth)
+ *
+ * Michael S. Horn
+ * Northwestern University
+ * michael-horn@northwestern.edu
+ * Copyright 2011, Michael S. Horn
+ *
+ * This project was funded by the National Science Foundation (grant 1010889).
+ * Any opinions, findings and conclusions or recommendations expressed in this
+ * material are those of the author(s) and do not necessarily reflect the views
+ * of the National Science Foundation (NSF).
+ */
+
+var Theme = {
+   
+   FOREGROUND : "white",
+   FADED  : "#6AB7DC",
+   TRANS_LIGHT  : "rgba(255, 255, 255, 0.7)",
+   TRANS_MEDIUM : "rgba(255, 255, 255, 0.5)",
+   TRANS_DARK   : "rgba(255, 255, 255, 0.3)",
+   GREEN  : "#33CC33",
+   PURPLE : "#993399",
+   ORANGE : "#FF6633"
+};
+
+var ThemeHMNH = {
+   
+   FOREGROUND : "white",
+   FADED : "#8f613c",
+   TRANS_LIGHT : "rgba(255, 255, 255, 0.7)",
+   TRANS_MEDIUM : "rgba(255, 255, 255, 0.5)",
+   TRANS_DARK : "rgba(255, 255, 255, 0.3)",
+   GREEN : "#2e9028",
+   PURPLE : "#993399",
+   ORANGE : "#c85119"
+};
